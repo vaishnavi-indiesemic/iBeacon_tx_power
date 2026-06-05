4d978e5381ee4ea1617d03ef160f2c225535e3ac
vaishnavi-indiesemic <vaishnavi1.indiesemic@gmail.com>
2026-06-05 10:17:50 +0530

    final code

--- src/main.c
+++ src/main.c
@@ -13,11 +13,12 @@
 #include <zephyr/bluetooth/hci.h>
 
 #ifndef IBEACON_RSSI
-#define IBEACON_RSSI 0xc8
+//-20
+#define IBEACON_RSSI 0x38
+//-40
+// #define IBEACON_RSSI 0x5a
 #endif
 
-#define DEVICE_NAME CONFIG_BT_DEVICE_NAME
-#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
 /*
  * Set iBeacon demo advertisement data. These values are for
  * demonstration only and must be changed for production environments!
@@ -34,15 +35,11 @@ static const struct bt_data ad[] = {
 	0x02, 0x15, /* iBeacon Type & Length */
 	0x4e, 0x49, 0x4b, 0x41, 0x54, 0x54, 0x45, 0x43, /* UUID */
 	0x48, 0x42, 0x45, 0x41, 0x43, 0x4f, 0x4e, 0x53, /* UUID */
-	0x00, 0x03, /* Index 20, 21: Major */
-	0x00, 0x01, /* Index 22, 23: Minor */
+	0x00, 0x04, /* Index 20, 21: Major */
+	0x00, 0x05, /* Index 22, 23:(1-15) Minor */
 	IBEACON_RSSI /* Calibrated RSSI @ 1m */
                 ),
 };
-static const struct bt_data sd[] = {
-    BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
-};
-
 static void bt_ready(int err)
 {
 	if (err) {
@@ -51,14 +48,20 @@ static void bt_ready(int err)
 	}
 
 	printk("Bluetooth initialized\n");
-
 	/* Start advertising */
-	err = bt_le_adv_start(BT_LE_ADV_NCONN, ad, ARRAY_SIZE(ad),
-			      sd, ARRAY_SIZE(sd));
-	if (err) {
-		printk("Advertising failed to start (err %d)\n", err);
-		return;
-	}
+	struct bt_le_adv_param adv_param = {
+        .options = BT_LE_ADV_OPT_NONE,
+        .interval_min = 1600, 
+        .interval_max = 1600,
+        .id = BT_ID_DEFAULT,
+    };
+
+    /* Start advertising using the custom interval parameters */
+    err = bt_le_adv_start(&adv_param, ad, ARRAY_SIZE(ad), NULL, 0);
+    if (err) {
+        printk("Advertising failed to start (err %d)\n", err);
+        return;
+    }
 
 	printk("iBeacon started\n");
 }
